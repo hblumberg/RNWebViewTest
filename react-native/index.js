de0e8b23d8d42d3aa73d73fa295004ec08c89a64
Hannah Blumberg <hblumberg@users.noreply.github.com>
2018-02-21 11:25:41 -0800

    Update naming.

--- react-native/index.js
+++ react-native/index.js
@@ -8,10 +8,10 @@ class ReactNativeScreen extends React.Component {
                 source={{uri: 'http://facebook.github.io/react-native/blog/'}}
                 onScrollChanged={({nativeEvent}) => {
                     console.log("Scroll changed:");
-                    console.log(nativeEvent.l);
-                    console.log(nativeEvent.t);
-                    console.log(nativeEvent.oldl);
-                    console.log(nativeEvent.oldt);
+                    console.log(nativeEvent.x);
+                    console.log(nativeEvent.y);
+                    console.log(nativeEvent.prevX);
+                    console.log(nativeEvent.prevY);
                 }}
             />
         );
